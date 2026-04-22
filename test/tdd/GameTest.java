3f10aa74a3afe92770d13dd1dfef8a781d2e57b7
orhanbagdatli <orhan.bagdatli@univ-tlse3.fr>
2026-04-22 23:51:34 +0200

    iter-2

--- test/tdd/GameTest.java
+++ test/tdd/GameTest.java
@@ -17,5 +17,13 @@ class GameTest {
 	void test_vide() {
 		assertEquals(0, game.score());
 	}
+	
+	@Test
+	void test_aucune_quille() {
+		for (int i = 0; i < 20; i++) {
+			game.roll(0);
+		}
+		assertEquals(0,game.score());
+	}
 
 }
